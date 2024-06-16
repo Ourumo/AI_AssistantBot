bb8662f9eefed15a05181193982fe2f973473f05
Ourumo <ss8638@naver.com>
2024-06-16 23:47:32 +0900

    main.py + prompt.txt

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from langchain_openai import ChatOpenAI
-import openai
-from dotenv import load_dotenv
+import openai # openai
+from langchain_openai import ChatOpenAI # langchain_openai
+from dotenv import load_dotenv # env 로드
+import subprocess # 터미널 사용
 
 load_dotenv()
+
+with open('prompt.txt', 'r') as file:
+    prompt = file.read()
+print(prompt)
+
+# 가능하다면 프로그램을 띄우는 느낌?
+# langchain_openai를 이용해서 gpt-3.5-turbo 사용
