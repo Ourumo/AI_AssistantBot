4ae6af67fc42cccc19a75215ae07455baa34148b
Ourumo <ss8638@naver.com>
2024-06-19 00:00:15 +0900

    langchain 간단 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openai # openai
+from langchain import LLMChain, PromptTemplate # langchain
 from langchain_openai import ChatOpenAI # langchain_openai
 from dotenv import load_dotenv # env 로드
 import subprocess # 터미널 사용
@@ -7,7 +8,19 @@ load_dotenv()
 
 with open('prompt.txt', 'r') as file:
     prompt = file.read()
-print(prompt)
+    print(prompt)
+    
+    
+chat = ChatOpenAI(model="gpt-3.5-turbo")
 
-# 가능하다면 프로그램을 띄우는 느낌?
+template = PromptTemplate(input_variables=["question"], template="Q: {question}\nA:")
+
+llm_chain = LLMChain(llm=chat, prompt=template)
+
+response = llm_chain.run({"question": "LangChain을 사용하여 OpenAI와 대화하는 방법은?"})
+
+print(response)
+
+
+# 가능하다면 프로그램을 띄우는 느낌? -> PyInstaller
 # langchain_openai를 이용해서 gpt-3.5-turbo 사용
