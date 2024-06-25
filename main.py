f01f99328002770a7f3d9b83c3543b3d2aa13f90
Ourumo <ss8638@naver.com>
2024-06-25 23:56:29 +0900

    feat: langchain 사용 추가

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-import openai # openai
-import os
-from langchain_openai import ChatOpenAI # langchain_openai
-from dotenv import load_dotenv # env 로드
-import getpass
-import subprocess # 터미널 사용
+from getpass import getpass
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 # 최초 로그인 시, API_Key 입력
 with open('.env', 'r') as env:
     key = env.read()
     key = key.replace("OPENAI_API_KEY=", "")
 if(key == ""):
-    key = getpass.getpass(prompt="API키를 입력해주세요: ")
+    key = getpass(prompt="API키를 입력해주세요: ")
     with open('.env', 'w') as env:
         env.write(f"OPENAI_API_KEY={key}")
-print(key)
+
+model = ChatOpenAI(model="gpt-3.5-turbo")
+
+prompt = ChatPromptTemplate.from_messages([
+    ("human", "Hello!!")
+])
+
+result = model.invoke(input="hello!!")
+
+print(result)
+
 
 
 # 가능하다면 프로그램을 띄우는 느낌? -> PyInstaller
